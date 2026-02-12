608fba392b6843f7102daccba1e22d8965dbd119
Vijayakash-588 <akashwolf0508@gmail.com>
2026-02-12 12:27:49 +0530

    updated jsx and css file

--- final-year-project/src/App.jsx
+++ final-year-project/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
     setIsLoggedIn(true);
   };
 
-<<<<<<< HEAD
   return (
     <Routes>
       {!isLoggedIn ? (
@@ -52,59 +51,6 @@ function App() {
         </>
       )}
     </Routes>
-=======
-  // ---------------- DASHBOARD ----------------//
-  if (isLoggedIn) {
-    return (
-      <div className="container">
-        <div className="dashboard-box">
-          <h1>Welcome {name} 🎉</h1>
-          <h2>Project Management System</h2>
-
-          <div className="module-container">
-            <div className="module-box">
-              <h2>Teams</h2>
-            </div>
-
-            <div className="module-box">
-              <h2>Reviews</h2>
-            </div>
-
-            <div className="module-box">
-              <h2>Marks</h2>
-            </div>
-
-            <div className="module-box">
-              <h2>Validation Status</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  // ---------------- LOGIN PAGE ----------------//
-  return (
-    <div className="container">
-      <div className="login-box">
-        <h1>Project Management System</h1>
-
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter Name"
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-
-          <input type="text" placeholder="Enter Department" required />
-          <input type="text" placeholder="Enter College" required />
-
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    </div>
->>>>>>> 855f23ffe184117b7eff0588cb68bc433fd7e4f4
   );
 }
 export default App;
